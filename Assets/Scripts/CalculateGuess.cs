0d6323bb57c9ca0bba68484495a651d3f938e873
Rahul Patwari <rahulpatwari233@gmail.com>
2019-02-05 01:14:36 +0530

    Calculate next guess
    Udemy: s04-42

---
+++ Assets/Scripts/CalculateGuess.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CalculateGuess : MonoBehaviour {
+	[SerializeField] int highLim = 1000;
+	[SerializeField] int lowLim = 1;
+	int currentGuess;
+	[SerializeField] public TextMeshProUGUI guessText;
+
+	private void Start () {
+		setText ();
+	}
+	public void onHighClicked () {
+		lowLim = currentGuess;
+		setText ();
+	}
+
+	public void onLowClicked () {
+		highLim = currentGuess;
+		setText ();
+	}
+
+	private void setText () {
+		currentGuess = (highLim + lowLim) / 2;
+		guessText.text = currentGuess.ToString ();
+	}
+}
